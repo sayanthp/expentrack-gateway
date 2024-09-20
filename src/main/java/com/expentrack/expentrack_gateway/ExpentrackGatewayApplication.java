646e572546eea61cb676bec5f13109c87ae5bc5c
sayanthp <psayanth.p@gmail.com>
2024-09-20 22:44:52 +0530

    Basic project created

---
+++ src/main/java/com/expentrack/expentrack_gateway/ExpentrackGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.expentrack.expentrack_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpentrackGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpentrackGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expentrack/expentrack_gateway/ExpentrackGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expentrack.expentrack_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpentrackGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
